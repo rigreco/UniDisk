c15fd5a0b9e00fa34d4ba8cf56814fa2e6653af0
Riccardo <rigreco.grc@gmail.com>
2015-02-14 16:14:35 +0100

    Update BtoFAC.s

--- FP-converter/BtoFAC.s
+++ FP-converter/BtoFAC.s
@@ -1,3 +1,20 @@
+*    Floating Point converter FAC to FP1 format
+*
+*    Copyright (C) 2015  Riccardo Greco
+
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*    
 * @com.wudsn.ide.asm.hardware=APPLE2
 ************************************
 * 	BASIC TO FAC TO FP1        *
@@ -22,7 +39,7 @@ FAC		equ $9D
 ENTRY		jsr CHKCOM
 		jsr FRMNUM	;VARIABLE->FAC
 
-** FP1 to FAC conversion (conversion not yet) **
+** FP1 to FAC conversion **
 
 		lda FAC
 		dec A
